3412b7589378415299edf264601145abac14cf40
konrad151 <konrad.k151@gmail.com>
2020-05-21 17:06:29 +0200

    Touch of style and header component

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import classNames from 'classnames'
+import styles from './Header.module.scss'
 
 const Header = () => {
-	return (
-		<>
-			<h2>Header</h2>
-		</>
-	);
-};
+	var shoppingCartClass = classNames(styles.header__navItem, styles.header__navItem_shoppingCart)
 
-export default Header;
+	return (
+		<header className={styles.header}>
+			<Container>
+				<Row>
+					<Col xs={12} sm={'auto'}>
+						<h1 className={styles.header__logo}>Star Wars</h1>
+					</Col>
+					<Col xs={12} sm>
+						<nav className={styles.header__nav}>
+							{/* uncomment below line and import Link from react-router-dom to add more link nav items */}
+							{/* <Link to={'#!'} className={styles.header__navItem}>TestNavLink</Link> */}
+							<div className={shoppingCartClass} onClick={() => console.log('clicked')}>shoppingCart</div>
+						</nav>
+					</Col>
+				</Row>
+			</Container>
+		</header>
+	)
+}
+
+export default React.memo(Header)
--- client/src/screens/Root.js
+++ client/src/screens/Root.js
@@ -4,10 +4,11 @@ import * as ROUTES from '../constants/routes'
 import Header from '../components/Header/Header'
 import Home from '../screens/Home/Home'
 import NotFound from '../screens/NotFound/NotFound'
+import styles from './Root.module.scss'
 
 const Root = () => {
 	return (
-		<>
+		<div className={styles.appWrapper}>
 			<BrowserRouter>
 				<Header />
 				<Switch>
@@ -15,7 +16,7 @@ const Root = () => {
 					<Route path={ROUTES.NOT_FOUND} component={NotFound} />
 				</Switch>
 			</BrowserRouter>
-		</>
+		</div>
 	)
 }
 
